78b6c94c3cdf11558321bae6c4a324b68de76a01
kravets-julia <kravets_julia@i.ua>
2023-01-27 13:54:38 +0200

    02-gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,20 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallaryContainer = document.querySelector('.gallery');
+const cardsGalery = (createGaleryItems(galleryItems));
+gallaryContainer.insertAdjacentHTML('beforeend', cardsGalery);
+console.log(gallaryContainer)
+
+gallaryContainer.addEventListener('click', onClickContainer)
+
+function createGaleryItems(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li>
+        <a class="gallery__link" href="${original}">
+          <img 
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            self
+          />
+        </a>
+        </li>
+      `;
+    }).join('');
+ }
 
 
+function onClickContainer(evt) {
+    evt.preventDefault();
 
+    if(evt.target.nodeName !== 'IMG') {
+        return
+      }
 
+ var lightbox = new SimpleLightbox('.gallery a', {
+    captions: "true",
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+    scrollZoom: false,
 
-// let Lightbox = new SimpleLightbox(".gallery a", {
-// captionData: "alt",
-// captionDelay: 250,
-// scrollZoom: false,
-// })
+ });
 
-// if(evt.target.nodeName === 'IMG') {
-//   close.SimpleLightbox;
-// }
+ if(evt.currentTarget.nodeName === 'IMG') {
+  close.SimpleLightbox;}
 
-// console.log(evt)
+ console.log(lightbox)
+
+}
